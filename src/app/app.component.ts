11b5e8c0f315eb51098c5f134e24f67ab12e0162
Minh Truong <minh@truong.fi>
2021-10-24 15:33:07 +0300

    First dev commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,18 @@
 import { Component } from '@angular/core';
+import {FormControl} from '@angular/forms';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
+declare interface BandInfo {
+  number: number,
+  name: string,
+  DLbandClass: string[],
+  ULbandCLass: string[],
+  sdl?: boolean;
+}
+
+export interface PolicyForm {
+  bandNumber: number[]
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +20,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'carrier-policy-generator';
+
+  faGithub = faGithub;
+
+  submitDisabled: boolean = false;
+
+  bandList: BandInfo[] = [
+    { number: 1, name: "Band 1", DLbandClass: ["A", "B", "C"], ULbandCLass: ["A"]},
+    { number: 3, name: "Band 3", DLbandClass: ["A", "B", "C"], ULbandCLass: ["A", "C"]},
+    { number: 7, name: "Band 7", DLbandClass: ["A", "B", "C", "D"], ULbandCLass: ["A"]},
+    { number: 8, name: "Band 8", DLbandClass: ["A", "B"], ULbandCLass: ["A"]},
+    { number: 20, name: "Band 20", DLbandClass: ["A"], ULbandCLass: ["A"]},
+    { number: 28, name: "Band 28", DLbandClass: ["A", "C"], ULbandCLass: ["A"]},
+    { number: 32, name: "Band 32", DLbandClass: ["A"], ULbandCLass: [], sdl: true }
+  ]
+
+  formData: PolicyForm = {
+    bandNumber: []
+  }
+
+  formResult: string |undefined;
+
+  submitForm() {
+    this.submitDisabled = true;
+
+    const bands = this.formData.bandNumber;
+
+    let bandsWithClass = [];
+    for(let band of bands) {
+      let bandWithClass = [];
+
+      let bandInfo = this.bandList.find(b => b.number == band)
+
+      let DLclasses = bandInfo?.DLbandClass
+      let ULclasses = bandInfo?.ULbandCLass
+
+      // Set default bandClass to A
+      if(DLclasses == undefined) {
+        DLclasses = ["A"]
+      }
+      if(ULclasses == undefined) {
+        ULclasses = []
+      }
+
+      for(let DLbandClass of DLclasses) {
+        for(let ULbandClass of ULclasses) {
+          bandWithClass.push(band.toString() + DLbandClass + ULbandClass)
+        }
+      }
+
+      bandsWithClass.push(bandWithClass)
+    }
+    console.log(bandsWithClass)
+
+    this.submitDisabled = false;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatButtonModule} from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatIconModule } from '@angular/material/icon';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +23,20 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatCheckboxModule,
+    HttpClientModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
